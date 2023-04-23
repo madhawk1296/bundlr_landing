6276acc3b7904a4fc1c69f1eee5dc26788fae4dd
madhawk1296 <silvacristian1296@gmail.com>
2023-04-23 17:01:04 -0600

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" href="/logoBlue.png" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" />
       </Head>
